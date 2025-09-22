45b805b37297c2045f1256eb06b302e5097b9518
TheMaykilaz <104752456+TheMaykilaz@users.noreply.github.com>
2025-09-22 19:47:24 +0300

    Draft main, where setting.py and url.py have been changed, and .gitignore env.sample has been added.

--- AirplaneDJ/settings.py
+++ AirplaneDJ/settings.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
-from password import dbpassword
+from dotenv import load_dotenv
 from datetime import timedelta
+
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zset1$z3j--8gh%hmv%lqb@@-kxvb1p=b^%5m=f8yia+$ta$u7'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -101,11 +103,11 @@ WSGI_APPLICATION = 'AirplaneDJ.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': BASE_DIR / 'db.airplane',
-        'USER': BASE_DIR / 'postgres',
-        'HOST': BASE_DIR / 'localhost',
-        'PORT': '5432',
-
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
     }
 }
 
