cae7575a663f1796e5e7e339b5c9a280bf8fd188
TheMaykilaz <104752456+TheMaykilaz@users.noreply.github.com>
2025-10-06 17:38:44 +0300

    Refactored a entire user model, added various key changes

--- user/serializers.py
+++ user/serializers.py
@@ -1,7 +1,20 @@
 from rest_framework import serializers
-from .models import User
+from .models import User, UserProfile, EmailVerificationCode, LoginAttempt
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from rest_framework import serializers as drf_serializers
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializer for UserProfile model"""
+    
+    class Meta:
+        model = UserProfile
+        fields = [
+            'passport_number', 'passport_expiry', 'passport_country',
+            'emergency_contact_name', 'emergency_contact_phone', 'emergency_contact_relationship',
+            'frequent_flyer_numbers', 'special_assistance',
+            'email_notifications', 'sms_notifications', 'marketing_emails'
+        ]
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -10,23 +23,34 @@ class UserSerializer(serializers.ModelSerializer):
         required=False,  # password is optional (for Google OAuth2 users)
         min_length=8
     )
+    profile = UserProfileSerializer(required=False)
+    full_name = serializers.ReadOnlyField()
+    age = serializers.ReadOnlyField()
+    booking_count = serializers.SerializerMethodField()
+    can_book_flights = serializers.ReadOnlyField()
 
     class Meta:
         model = User
-        fields = (
-            "id",
-            "email",
-            "username",
-            "password",
-            "role",
-            "first_name",
-            "last_name",
-            "phone",
-            "created_at",
-        )
-        read_only_fields = ["created_at"]
+        fields = [
+            "id", "email", "username", "password", "role",
+            "first_name", "last_name", "phone", "date_of_birth", "gender",
+            "address_line1", "address_line2", "city", "state_province", 
+            "postal_code", "country", "preferred_seat_class", "dietary_restrictions",
+            "is_email_verified", "is_phone_verified", "google_id",
+            "created_at", "updated_at", "full_name", "age", "booking_count",
+            "can_book_flights", "profile"
+        ]
+        read_only_fields = ["created_at", "updated_at", "id"]
+        extra_kwargs = {
+            'password': {'write_only': True},
+            'google_id': {'read_only': True},
+        }
+
+    def get_booking_count(self, obj):
+        return obj.get_booking_count()
 
     def create(self, validated_data):
+        profile_data = validated_data.pop("profile", {})
         password = validated_data.pop("password", None)
         req = self.context.get("request")
 
@@ -38,9 +62,15 @@ class UserSerializer(serializers.ModelSerializer):
         if password:  # Set password only if provided
             user.set_password(password)
             user.save()
+        
+        # Create profile if data provided
+        if profile_data:
+            UserProfile.objects.create(user=user, **profile_data)
+        
         return user
 
     def update(self, instance, validated_data):
+        profile_data = validated_data.pop("profile", {})
         password = validated_data.pop("password", None)
         req = self.context.get("request")
 
@@ -48,7 +78,7 @@ class UserSerializer(serializers.ModelSerializer):
         if "role" in validated_data and (not req or not getattr(req.user, "is_staff", False)):
             validated_data.pop("role")
 
-        # Update other fields
+        # Update user fields
         for attr, value in validated_data.items():
             setattr(instance, attr, value)
 
@@ -56,6 +86,14 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
 
         instance.save()
+        
+        # Update or create profile
+        if profile_data:
+            profile, created = UserProfile.objects.get_or_create(user=instance)
+            for attr, value in profile_data.items():
+                setattr(profile, attr, value)
+            profile.save()
+        
         return instance
 
 
@@ -111,3 +149,50 @@ class EmailLoginVerifySerializer(serializers.Serializer):
     """Serializer for verifying the email code and logging in."""
     email = serializers.EmailField(required=True)
     code = serializers.CharField(required=True, min_length=6, max_length=6)
+
+
+class UserPublicSerializer(serializers.ModelSerializer):
+    """Public user serializer with limited fields for display"""
+    full_name = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = User
+        fields = ['id', 'full_name', 'first_name', 'last_name', 'created_at']
+        read_only_fields = ['id', 'created_at']
+
+
+class UserStatsSerializer(serializers.ModelSerializer):
+    """User statistics serializer for admin/analytics"""
+    full_name = serializers.ReadOnlyField()
+    age = serializers.ReadOnlyField()
+    booking_count = serializers.SerializerMethodField()
+    completed_flights = serializers.SerializerMethodField()
+    can_book_flights = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = User
+        fields = [
+            'id', 'email', 'full_name', 'role', 'age', 'country',
+            'preferred_seat_class', 'is_email_verified', 'is_phone_verified',
+            'booking_count', 'completed_flights', 'can_book_flights',
+            'created_at', 'last_login'
+        ]
+        read_only_fields = ['id', 'created_at', 'last_login']
+    
+    def get_booking_count(self, obj):
+        return obj.get_booking_count()
+    
+    def get_completed_flights(self, obj):
+        return obj.get_completed_flights()
+
+
+class LoginAttemptSerializer(serializers.ModelSerializer):
+    """Serializer for login attempts (admin use)"""
+    
+    class Meta:
+        model = LoginAttempt
+        fields = [
+            'id', 'email', 'attempt_type', 'status', 'ip_address',
+            'failure_reason', 'created_at'
+        ]
+        read_only_fields = ['id', 'created_at']
